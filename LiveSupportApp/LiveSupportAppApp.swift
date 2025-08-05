63c787324c2997af54241d259c56d7f3b07bf2cc
Furkan Karakoc <44609894+furkankarakoc@users.noreply.github.com>
2025-08-05 12:54:01 +0300

    Initial Commit

---
+++ LiveSupportApp/LiveSupportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSupportAppApp.swift
+//  LiveSupportApp
+//
+//  Created by furkankarakoc on 5.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveSupportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveSupportAppTests/LiveSupportAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSupportAppTests.swift
+//  LiveSupportAppTests
+//
+//  Created by furkankarakoc on 5.08.2025.
+//
+
+import Testing
+@testable import LiveSupportApp
+
+struct LiveSupportAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
